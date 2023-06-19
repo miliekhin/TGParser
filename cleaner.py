fcda6e5fc946dd516553f4041f759d3397b31cfa
Aleksey Miliekhin <miliekhin@printum.io>
2023-06-20 00:49:31 +0300

    add clean gruz by pattern

--- cleaner.py
+++ cleaner.py
@@ -7,8 +7,51 @@ def clean_trucks(msg):
     # if match_obj:
     #     print('[clean_trucks] Can\'t clean trucks as found digits in message.')
     #     return msg
+    msg = clean_trucks_by_pattern(msg)
     msg = re.sub(GRUZ_CLEAN, '', msg).replace('  ', ' ')
     print('[clean_trucks] Clean trucks result:', msg)
+    return msg
+
+
+def clean_trucks_by_pattern(msg):
+    p1 = fr'грузовы[хе].{{1,10}}(мост|пост|заправ|луко|столов|поворот|гостиниц|кафе|душевы|мойк|азс|склад|овощ|спуск)'
+    p2 = fr'(грузовы[хе]|фур) (шт\.?(ук)?|машин)? ?({D_RANGE}|нет|ноль|полоса пустая)'
+    p3 = fr'{D_RANGE} фур'
+    ptrns = (
+        {
+            'ptrn_gruz': p1,
+            'ptrn_full': fr'легковы[хе].+{p1}',
+        },
+        {
+            'ptrn_gruz': p2,
+            'ptrn_full': fr'{D_RANGE} (машин|авто(мобилей)?)? ?легковы[хе].+{p2}',
+        },
+        {
+            'ptrn_gruz': p2,
+            'ptrn_full': fr'легковы[хе] машин {D_RANGE}.+{p2}',
+        },
+        {
+            'ptrn_gruz': p2,
+            'ptrn_full': fr'{p2}.+легковы[хе]',
+        },
+        {
+            'ptrn_gruz': p3,
+            'ptrn_full': fr'{p3}.+{D_RANGE} легковы',
+        },
+    )
+
+    found = None
+    for ptrn in ptrns:
+        res = re.search(ptrn['ptrn_full'], msg)
+        if res:
+            sp = res.span()
+            found = msg[sp[0]: sp[1]]
+            msg = re.sub(ptrn['ptrn_gruz'], '', msg).replace('  ', ' ')
+            break
+
+    if found:
+        print('[clean_trucks_by_pattern] pattern found:', found)
+
     return msg
 
 
